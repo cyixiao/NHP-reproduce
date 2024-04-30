be782f71e67de4a7e773a35d6916c63784b1b561
cyixiao <ychen3@unc.edu>
2024-04-30 04:46:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
 import torch
 from torch.utils.data import DataLoader
+import scipy.sparse as sp
 
 from dataloader import Hypergraph
 from model_NHP import NHP
 from test import test_model
 from train import train
 
-feature_size = 32
+dataset = 'iJO1366'
+feature_size = sp.load_npz(f"datasets/{dataset}/features.npz").shape[1]
 hidden_size = 512
 g_func = 'maxmin'
-dataset = 'iAF1260b'
 split = 'train'
 batch_size = 64
 epochs = 50
 learning_rate = 0.001
-k = 700
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # initialize model
 model = NHP(feature_size, hidden_size, g_func)
 
 # load the dataset
-train_hypergraph = Hypergraph(dataset, split, feature_size, batch_size)
-test_hypergraph = Hypergraph(dataset, "test", feature_size, batch_size)
+train_hypergraph = Hypergraph(dataset, split, batch_size)
+test_hypergraph = Hypergraph(dataset, "test", batch_size)
 train_data = DataLoader(train_hypergraph, batch_size=1, shuffle=False)
 test_data = DataLoader(test_hypergraph, batch_size=1, shuffle=False)
 
